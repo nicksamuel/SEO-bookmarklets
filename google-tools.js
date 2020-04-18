5a7bda38e6b50c29fbe7be4dc58b494fd57273d4
Nick Samuel <nicksamuel@users.noreply.github.com>
2020-04-18 18:59:55 +0100

    Update google-tools.js

--- google-tools.js
+++ google-tools.js
@@ -1,21 +1,26 @@
-// These are all based on individual testing tools from Google.
+// These are five fairly popular tools from Google which can be used outside of Search Console and Lighthouse:
 
-// Pagespeed Insights
+// As from the Detailed.com guide, change window.location. to window.location.host. if you always want to run the tests on the homepage.
+
+
+// 1. Pagespeed Insights
 
 javascript:location.href='https://developers.google.com/speed/pagespeed/insights/?url='+window.location
 
-// Structured Data Testing Tool
-
-javascript:location.href='https://search.google.com/structured-data/testing-tool/u/0/#url='+window.location
-
-// Mobile Friendly Test
+// 2. Mobile Friendly Test
 
 javascript:location.href='https://search.google.com/test/mobile-friendly?url='+window.location
 
-// AMP Test - Nick
+// 3. Structured Data Testing Tool
+
+javascript:location.href='https://search.google.com/structured-data/testing-tool/u/0/#url='+window.location
+
+// 4. AMP Test - Nick
 
 javascript:location.href='https://search.google.com/test/amp?url='+window.location
 
-// Rich Results - Nick
+// 5. Rich Results - Nick
 
 javascript:location.href='https://search.google.com/test/rich-results?url='+window.location
+
+
