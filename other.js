977e51d09adb39ef77662fddd8531555a6803494
Nick Samuel <nicksamuel@users.noreply.github.com>
2020-04-18 19:25:48 +0100

    Update other.js

--- other.js
+++ other.js
@@ -1,3 +1,20 @@
-// title
+// List of other tools for SEO.
 
-// example
+// 1. Siteliner - Detailed.com
+
+javascript:location.href='https://www.siteliner.com/'+ window.location.host
+
+
+// 2. SimilarWeb (current tab and new tab) - Detailed.com
+
+javascript:location.href='https://similarweb.com/website/'+location.host
+
+javascript:window.open('https://similarweb.com/website/'+window.location.host);
+
+
+// 3. Archive.org AKA Way Back Machine (Current URL and Current Domain) - Detailed.com
+
+javascript:location.href='https://web.archive.org/web/*/'+ window.location.href
+
+javascript:location.href='https://web.archive.org/web/*/'+ window.location.host
+
