86f8a2ebc6f5e8891232ba46fde05abb8a354d25
Nick Samuel <nicksamuel@users.noreply.github.com>
2020-04-18 19:28:35 +0100

    Update google-index.js

--- google-index.js
+++ google-index.js
@@ -5,7 +5,16 @@
 javascript:window.open('https://www.google.com/search?q=site%3A%27+window.location.hostname
 +%27 inurl:demo OR inurl:/home/ OR inurl:test OR inurl:-2 OR inurl:screenshot OR inurl:sample OR inurl:portfolio OR inurl:landing');
                        
+                       
+                       
 // 2. Find non https domains/subdomains etc.- Detailed.com
                        
 javascript:window.location.href='https://www.google.com/search?q=site%3A%27+window.location.hostname+%27 -inurl:https'
+                       
+                       
+                       
+// 3. View Google Web Cache - Detailed.com
+                       
+javascript:location.href='https://webcache.googleusercontent.com/search?q=cache:’+window.location              
+                       
                        
