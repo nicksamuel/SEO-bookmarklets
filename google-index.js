4831bb9bf1e7c931d45e57188702c5ebba99f423
Nick Samuel <nicksamuel@users.noreply.github.com>
2020-04-18 19:18:26 +0100

    Update google-index.js

--- google-index.js
+++ google-index.js
@@ -1,11 +1,11 @@
 // Examples of how to perform quick Google indexation checks for site audits etc.
 
-// 1. Detailed.com - Find common site issues.
+// 1. Find common site issues - Detailed.com
 
 javascript:window.open('https://www.google.com/search?q=site%3A%27+window.location.hostname
 +%27 inurl:demo OR inurl:/home/ OR inurl:test OR inurl:-2 OR inurl:screenshot OR inurl:sample OR inurl:portfolio OR inurl:landing');
                        
-// 2. Detailed.com - Find non https domains/subdomains etc.
+// 2. Find non https domains/subdomains etc.- Detailed.com
                        
 javascript:window.location.href='https://www.google.com/search?q=site%3A%27+window.location.hostname+%27 -inurl:https'
                        
