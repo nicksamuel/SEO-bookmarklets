026e27901b2cf76569ee7a22c072f5ef7f355dc0
Nick Samuel <nicksamuel@users.noreply.github.com>
2020-04-18 18:47:49 +0100

    Update seo-tools.js

--- seo-tools.js
+++ seo-tools.js
@@ -1,5 +1,40 @@
-// title
+// PAGE LEVEL METRICS
 
-// Tool 1
+// Ahrefs
 
-// etc
+javascript:location.href='https://ahrefs.com/site-explorer/overview/v2/subdomains/recent?target='+encodeURIComponent(location.hostname + location.pathname)
+
+// Majestic
+
+javascript:location.href='https://majestic.com/reports/site-explorer?folder=&q='+window.location.href
+
+// Moz
+
+javascript:location.href='https://analytics.moz.com/pro/link-explorer/overview?site='+window.location.href
+
+// SEMRush
+
+javascript:location.href='https://www.semrush.com/info/'+window.location.href
+
+
+
+
+
+// DOMAIN LEVEL METRICS
+
+// Ahrefs - need to fix this one.
+
+javascript:location.href='https://ahrefs.com/site-explorer/overview/v2/subdomains/recent?target='+encodeURIComponent(location.hostname + location.pathname)
+
+// Majestic
+
+javascript:location.href='https://majestic.com/reports/site-explorer?folder=&q='+window.location.host
+
+// Moz
+
+javascript:location.href='https://analytics.moz.com/pro/link-explorer/overview?site='+window.location.host
+
+// SEMRush
+
+javascript:location.href='https://www.semrush.com/info/'+window.location.host
+
