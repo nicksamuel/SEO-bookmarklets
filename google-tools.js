06276197ffb5a00420c577423fd11d907bbb5fa7
Nick Samuel <nicksamuel@users.noreply.github.com>
2020-04-18 19:00:20 +0100

    Update google-tools.js

--- google-tools.js
+++ google-tools.js
@@ -7,17 +7,21 @@
 
 javascript:location.href='https://developers.google.com/speed/pagespeed/insights/?url='+window.location
 
+
 // 2. Mobile Friendly Test
 
 javascript:location.href='https://search.google.com/test/mobile-friendly?url='+window.location
+
 
 // 3. Structured Data Testing Tool
 
 javascript:location.href='https://search.google.com/structured-data/testing-tool/u/0/#url='+window.location
 
+
 // 4. AMP Test - Nick
 
 javascript:location.href='https://search.google.com/test/amp?url='+window.location
+
 
 // 5. Rich Results - Nick
 
