e2d5139ea6ffc7395d404e16fa0b1db9a9f77935
Nick Samuel <nicksamuel@users.noreply.github.com>
2020-04-18 19:18:53 +0100

    Update google-tools.js

--- google-tools.js
+++ google-tools.js
@@ -3,17 +3,17 @@
 // As per the Detailed.com guide, change window.location. to window.location.host. if you want to automatically run the tests on the homepage
 
 
-// 1. Pagespeed Insights
+// 1. Pagespeed Insights - Detailed.com
 
 javascript:location.href='https://developers.google.com/speed/pagespeed/insights/?url='+window.location
 
 
-// 2. Mobile Friendly Test
+// 2. Mobile Friendly Test - Detailed.com
 
 javascript:location.href='https://search.google.com/test/mobile-friendly?url='+window.location
 
 
-// 3. Structured Data Testing Tool
+// 3. Structured Data Testing Tool - Detailed.com
 
 javascript:location.href='https://search.google.com/structured-data/testing-tool/u/0/#url='+window.location
 
