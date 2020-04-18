e3a3c3e9b8d503b0a5d4f70690e72c9d6d0cad23
Nick Samuel <nicksamuel@users.noreply.github.com>
2020-04-18 20:48:16 +0100

    Update google-index.js

--- google-index.js
+++ google-index.js
@@ -14,14 +14,17 @@ javascript:window.location.href='https://www.google.com/search?q=site%3A%27+wind
 // 3. View Google Web Cache - SEO Cipher - NEED TO FIX.
                                           
 javascript:(function()%7B%20window.open(%27http://webcache.googleusercontent.com/search%3Fq%3Dcache:%27%2BencodeURIComponent(location.href))%7D)()%3B      
-                       
+          
+                                        
 // 4. View Google Cache for last 7 days - Cognitive SEO
                        
 javascript:(function(){%20window.open('http://www.google.co.uk/search?q=site%3A'+encodeURIComponent(location.host)+'&tbs=qdr:d7')})();                    
-                       
+             
+
 // 5. Single Page indexation checker - Cognitive SEO
 
 javascript:(function(){%20window.open('http://www.google.co.uk/search?q=site%3A'+encodeURIComponent(location.href))})();
+
 
 // 6. Quick Site Index - Cognitive SEO
 
