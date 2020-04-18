73ba98af0e8741882e8770821a4a82063a241fde
Nick Samuel <nicksamuel@users.noreply.github.com>
2020-04-18 18:57:04 +0100

    Update google-tools.js

--- google-tools.js
+++ google-tools.js
@@ -1,11 +1,21 @@
 // These are all based on individual testing tools from Google.
 
-// Example 1
+// Pagespeed Insights
 
-// Example 2
+javascript:location.href='https://developers.google.com/speed/pagespeed/insights/?url='+window.location
 
-// Example 3
+// Structured Data Testing Tool
 
-// Example 4
+javascript:location.href='https://search.google.com/structured-data/testing-tool/u/0/#url='+window.location
 
-// Example 5
+// Mobile Friendly Test
+
+javascript:location.href='https://search.google.com/test/mobile-friendly?url='+window.location
+
+// AMP Test - Nick
+
+javascript:location.href='https://search.google.com/test/amp?url='+window.location
+
+// Rich Results - Nick
+
+javascript:location.href='https://search.google.com/test/rich-results?url='+window.location
