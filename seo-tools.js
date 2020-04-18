c82e1a533adad3b4b2746edee3ea0f094628f647
Nick Samuel <nicksamuel@users.noreply.github.com>
2020-04-18 20:49:32 +0100

    Update seo-tools.js

--- seo-tools.js
+++ seo-tools.js
@@ -4,15 +4,19 @@
 
 javascript:location.href='https://ahrefs.com/site-explorer/overview/v2/subdomains/recent?target='+encodeURIComponent(location.hostname + location.pathname)
 
+
 // Majestic
 
 javascript:location.href='https://majestic.com/reports/site-explorer?folder=&q='+window.location.href
+
 
 // Moz
 
 javascript:location.href='https://analytics.moz.com/pro/link-explorer/overview?site='+window.location.href
 
+
 // SEMRush
+
 
 javascript:location.href='https://www.semrush.com/info/'+window.location.href
 
@@ -26,17 +30,21 @@ javascript:location.href='https://www.semrush.com/info/'+window.location.href
 
 javascript:location.href='https://ahrefs.com/site-explorer/overview/v2/subdomains/recent?target='+encodeURIComponent(location.hostname + location.pathname)
 
+
 // Majestic
 
 javascript:location.href='https://majestic.com/reports/site-explorer?folder=&q='+window.location.host
+
 
 // Moz
 
 javascript:location.href='https://analytics.moz.com/pro/link-explorer/overview?site='+window.location.host
 
+
 // SEMRush
 
 javascript:location.href='https://www.semrush.com/info/'+window.location.host
+
 
 // Search Metrics - Cognitive SEO
 
