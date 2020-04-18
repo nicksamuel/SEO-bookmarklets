04bb283445d92df6fba0c3327bbe0f27f60e2d6d
Nick Samuel <nicksamuel@users.noreply.github.com>
2020-04-18 20:51:58 +0100

    Update seo-tools.js

--- seo-tools.js
+++ seo-tools.js
@@ -1,51 +1,31 @@
 // PAGE LEVEL METRICS - Detailed.com unless otherwise stated
 
-// Ahrefs
+// 1. Ahrefs
 
 javascript:location.href='https://ahrefs.com/site-explorer/overview/v2/subdomains/recent?target='+encodeURIComponent(location.hostname + location.pathname)
 
 
-// Majestic
+// 2. Majestic
 
 javascript:location.href='https://majestic.com/reports/site-explorer?folder=&q='+window.location.href
-
-
-// Moz
-
-javascript:location.href='https://analytics.moz.com/pro/link-explorer/overview?site='+window.location.href
-
-
-// SEMRush
-
-
-javascript:location.href='https://www.semrush.com/info/'+window.location.href
-
-
-
-
-
-// DOMAIN LEVEL METRICS - Detailed.com unless otherwise stated
-
-// Ahrefs - need to fix this one.
-
-javascript:location.href='https://ahrefs.com/site-explorer/overview/v2/subdomains/recent?target='+encodeURIComponent(location.hostname + location.pathname)
-
-
-// Majestic
 
 javascript:location.href='https://majestic.com/reports/site-explorer?folder=&q='+window.location.host
 
 
-// Moz
+// 3. Moz
+
+javascript:location.href='https://analytics.moz.com/pro/link-explorer/overview?site='+window.location.href
 
 javascript:location.href='https://analytics.moz.com/pro/link-explorer/overview?site='+window.location.host
 
 
-// SEMRush
+// 4. SEMRush
+
+javascript:location.href='https://www.semrush.com/info/'+window.location.href
 
 javascript:location.href='https://www.semrush.com/info/'+window.location.host
 
 
-// Search Metrics - Cognitive SEO
+// 5. Search Metrics - Cognitive SEO
 
 javascript:(function(){%20window.open('http://suite.searchmetrics.com/en/research?acc=0&url='+location.hostname)})();
